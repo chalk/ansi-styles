1d1e9f26a4295d59b2e5b21410d3c9728bf2809c
Sindre Sorhus <sindresorhus@gmail.com>
2013-08-12 21:05:13 +0200

    Improve screenshot

--- test.js
+++ test.js
@@ -4,20 +4,18 @@ var assert = require('assert');
 var ansi = require('./ansi-styles');
 
 // generates the screenshot
-console.log(ansi.green[0] + 'Styles:' + ansi.green[1] + '\n');
-
 Object.keys(ansi).forEach(function (el) {
 	var style = ansi[el][0];
 
 	if (el === 'reset') {
-		style = '';
+		return;
 	}
 
 	if (/^bg[^B]/.test(el)) {
 		style = ansi.black[0] + style;
 	}
 
-	process.stdout.write(style + el + ansi.reset + ' ');
+	process.stdout.write(style + el + ansi.reset[0] + ansi.reset[1] + ' ');
 });
 
 describe('ansiStyles()', function () {
