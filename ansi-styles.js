70292a8c0f7712c1363aaed2f929af51439545a7
Mikito Takada <mikito.takada@gmail.com>
2014-05-08 16:19:10 -0700

    Add dim, blink and hidden
    dim, blink and hidden are supported by some terminals: http://misc.flogisoft.com/bash/tip_colors_and_formatting

--- ansi-styles.js
+++ ansi-styles.js
@@ -5,9 +5,12 @@ var codes = {
 	reset: [0, 0],
 
 	bold: [1, 22],
+	dim: [ 2, 22 ],
 	italic: [3, 23],
 	underline: [4, 24],
+	blink: [ 5, 25 ],
 	inverse: [7, 27],
+	hidden: [ 8, 28 ],
 	strikethrough: [9, 29],
 
 	black: [30, 39],
