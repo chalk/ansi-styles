b5cc0cd6d6333458a366c44d0eb5a72c585c5775
Sindre Sorhus <sindresorhus@gmail.com>
2015-12-13 18:22:18 +0100

    fix tests

--- test.js
+++ test.js
@@ -16,30 +16,26 @@ for (const [key, val] of Object.entries(ansi)) {
 	process.stdout.write(style + key + ansi.reset.close + ' ');
 }
 
-test('should return ANSI escape codes', t => {
+test('return ANSI escape codes', t => {
 	t.is(ansi.green.open, '\u001b[32m');
 	t.is(ansi.bgGreen.open, '\u001b[42m');
 	t.is(ansi.green.close, '\u001b[39m');
 	t.is(ansi.gray.open, ansi.grey.open);
-	t.end();
 });
 
-test('should group related codes into categories', t => {
+test('group related codes into categories', t => {
 	t.is(ansi.colors.magenta, ansi.magenta);
 	t.is(ansi.bgColors.bgYellow, ansi.bgYellow);
 	t.is(ansi.modifiers.bold, ansi.bold);
-	t.end();
 });
 
 test('groups should not be enumerable', t => {
-	t.false(Object.keys(ansi).includes('modifiers'));
-	t.end();
+	t.true(Object.keys(ansi).indexOf('modifiers') === -1);
 });
 
-test('should not pollute other objects', t => {
+test('don\'t pollute other objects', t => {
 	const obj1 = require('./');
 	const obj2 = require('./');
 	obj1.foo = true;
 	t.not(obj1.foo, obj2.foo);
-	t.end();
 });
