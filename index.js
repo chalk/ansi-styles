35855debc526801ffa748c8d6d1121015fcd385f
bluelovers <codelovers@users.sourceforge.net>
2017-03-24 10:10:01 +0800

    Revert "Revert "add raw data for who wanna re-use source data""
    This reverts commit 70364e7fd4f91a24c506d76f094342fa0562211c.

--- index.js
+++ index.js
@@ -84,6 +84,11 @@ function assembleStyles() {
 				open: `\u001B[${style[0]}m`,
 				close: `\u001B[${style[1]}m`
 			};
+
+			Object.defineProperty(styles[styleName], 'data', {
+				value: style,
+				enumerable: false
+			});
 		});
 
 		Object.defineProperty(styles, groupName, {
