6498e1311259742bd5f4d4a63ce04daf48c25e43
Mario Nebl <marionebl@users.noreply.github.com>
2018-01-29 18:23:04 +0100

    Use SVG screenshot (#44)

---
+++ screenshot.js
@@ -0,0 +1,27 @@
+'use strict';
+const style = require('.');
+
+const width = 55;
+let lineLength = 0;
+
+for (const [key, val] of Object.entries(style)) {
+	let code = val.open;
+	let projectedLength = lineLength + key.length + 1;
+
+	if (key === 'reset' || key === 'hidden' || key === 'grey' || key.includes('Bright')) {
+		continue;
+	}
+
+	if (/^bg[^B]/.test(key)) {
+		code = style.black.open + code;
+	}
+
+	if (width < projectedLength) {
+		process.stdout.write('\n');
+		lineLength = 0;
+		projectedLength = key.length + 1;
+	}
+
+	process.stdout.write(code + key + style.reset.close + ' ');
+	lineLength = projectedLength;
+}
