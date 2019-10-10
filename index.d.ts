78f7ed9881fa15aab85e5f0cd5811dd70e6b2e6e
Sindre Sorhus <sindresorhus@gmail.com>
2019-10-10 11:32:41 +0700

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -3,122 +3,128 @@ import * as cssColors from 'color-name';
 declare namespace ansiStyles {
 	interface ColorConvert {
 		/**
-		The RGB color space
+		The RGB color space.
 
-		@param red (0-255)
-		@param green (0-255)
-		@param blue (0-255)
+		@param red - (0-255).
+		@param green - (0-255).
+		@param blue - (0-255).
 		*/
 		rgb(red: number, green: number, blue: number): string;
 
 		/**
-		The HSL color space
+		The HSL color space.
 
-		@param hue (0-360)
-		@param saturation (0-100)
-		@param lightness (0-100)
+		@param hue - (0-360).
+		@param saturation - (0-100).
+		@param lightness - (0-100).
 		*/
 		hsl(hue: number, saturation: number, lightness: number): string;
 
 		/**
-		The HSV color space
+		The HSV color space.
 
-		@param hue (0-360)
-		@param saturation (0-100)
-		@param value (0-100)
+		@param hue - (0-360).
+		@param saturation - (0-100).
+		@param value - (0-100).
 		*/
 		hsv(hue: number, saturation: number, value: number): string;
 
 		/**
-		The HSV color space
+		The HSV color space.
 
-		@param hue (0-360)
-		@param whiteness (0-100)
-		@param blackness (0-100)
+		@param hue - (0-360).
+		@param whiteness - (0-100).
+		@param blackness - (0-100).
 		*/
 		hwb(hue: number, whiteness: number, blackness: number): string;
 
 		/**
-		The CMYK color space
+		The CMYK color space.
 
-		@param cyan (0-100)
-		@param magenta (0-100)
-		@param yellow (0-100)
-		@param black (0-100)
+		@param cyan - (0-100).
+		@param magenta - (0-100).
+		@param yellow - (0-100).
+		@param black - (0-100).
 		*/
 		cmyk(cyan: number, magenta: number, yellow: number, black: number): string;
 
 		/**
-		The XYZ color space
+		The XYZ color space.
 
-		@param x (0-100)
-		@param y (0-100)
-		@param z (0-100)
+		@param x - (0-100).
+		@param y - (0-100).
+		@param z - (0-100).
 		*/
 		xyz(x: number, y: number, z: number): string;
 
 		/**
-		The LAB color space
+		The LAB color space.
 
-		@param x (0-100)
-		@param y (0-100)
-		@param z (0-100)
+		@param x - (0-100).
+		@param y - (0-100).
+		@param z - (0-100).
 		*/
 		lab(lightness: number, a: number, b: number): string;
 
 		/**
-		The LCH/HCL color space
+		The LCH/HCL color space.
 
-		@param lightness (0-100)
-		@param chroma (0-100)
-		@param hue (0-360)
+		@param lightness - (0-100).
+		@param chroma - (0-100).
+		@param hue - (0-360).
 		*/
 		lch(lightness: number, chroma: number, hue: number): string;
 
 		/**
-		The RGB HEX color space
+		The RGB HEX color space.
 
-		@param hex A hexadecimal string containing RGB data
+		@param hex - A hexadecimal string containing RGB data.
 		*/
 		hex(hex: string): string;
 
-		/** @param keyword A CSS color name */
+		/**
+		@param keyword - A CSS color name.
+		*/
 		keyword(keyword: keyof typeof cssColors): string;
 
 		ansi(ansi: number): string;
 		ansi256(ansi: number): string;
 
 		/**
-		The HCG color space
+		The HCG color space.
 
-		@param hue (0-360)
-		@param chroma (0-100)
-		@param gray (0-100)
+		@param hue - (0-360).
+		@param chroma - (0-100).
+		@param gray - (0-100).
 		*/
 		hcg(hue: number, chroma: number, gray: number): string;
 
 		/**
-		The Apple RGB 16-bit color space
+		The Apple RGB 16-bit color space.
 
-		@param red (0-65535)
-		@param green (0-65535)
-		@param blue (0-65535)
+		@param red - (0-65535).
+		@param green - (0-65535).
+		@param blue - (0-65535).
 		*/
 		apple(r16: number, g16: number, b16: number): string;
 
 		/**
-		Grayscale color
+		Grayscale color.
 
-		@param gray (0-100)
+		@param gray - (0-100).
 		*/
 		gray(gray: number): string;
 	}
 
 	interface CSIPair {
-		/** The ANSI terminal Control Sequence for starting this style */
+		/**
+		The ANSI terminal control sequence for starting this style.
+		*/
 		readonly open: string;
 
-		/** The ANSI terminal Control Sequence for ending this style */
+		/**
+		The ANSI terminal control sequence for ending this style.
+		*/
 		readonly close: string;
 	}
 
@@ -127,7 +133,9 @@ declare namespace ansiStyles {
 		readonly ansi256: ColorConvert;
 		readonly ansi16m: ColorConvert;
 
-		/** The ANSI terminal Control Sequence for ending this color */
+		/**
+		The ANSI terminal control sequence for ending this color.
+		*/
 		readonly close: Close;
 	}
 
